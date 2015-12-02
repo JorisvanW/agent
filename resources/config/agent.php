347a639c44bb5f7954b3fe51535f799e1a7fad21
Joris van Willigen <j-_is@hotmail.com>
2015-12-02 15:45:52 +0100

    Fixed phpcs

--- resources/config/agent.php
+++ resources/config/agent.php
@@ -9,41 +9,41 @@
  */
 return [
 
-	/**
+    /*
      * List of desktop devices.
      *
-     * 'Macintosh'   => 'Macintosh'
+     * 'Macintosh'     => 'Macintosh'
      */
-	'devices' => [],
-	
-	/**
-	 * List of additional operating systems.
-	 *
-	 * 'OS X' 		  => 'Mac OS X'
-	 */ 
-	'operatingSystems' => [],
-	
-	/**
+    'devices'          => [],
+
+    /*
+     * List of additional operating systems.
+     *
+     * 'OS X' 		   => 'Mac OS X'
+     */
+    'operatingSystems' => [],
+
+    /*
      * List of additional browsers.
      *
-     * 'Safari' 	  => 'Safari',
-     * 'IE'           => 'MSIE|IEMobile|MSIEMobile|Trident/[.0-9]+',
+     * 'Safari' 	   => 'Safari',
+     * 'IE'            => 'MSIE|IEMobile|MSIEMobile|Trident/[.0-9]+',
      */
-	'browsers' => [],
-	
-	/**
+    'browsers'         => [],
+
+    /*
      * List of additional properties.
      *
-     * 'OS X'         => 'OS X [VER]'
-     * 'BlackBerryOS' => ['BlackBerry[\w]+/[VER]', 'BlackBerry.*Version/[VER]', 'Version/[VER]'],
+     * 'OS X'          => 'OS X [VER]'
+     * 'BlackBerryOS'  => ['BlackBerry[\w]+/[VER]', 'BlackBerry.*Version/[VER]', 'Version/[VER]'],
      */
-	'properties' => [],
-	
-	/**
+    'properties'       => [],
+
+    /*
      * List of robots.
      *
      * 'Google'        => 'googlebot'
      */
-	'robots' => []
+    'robots'           => []
 
 ];
